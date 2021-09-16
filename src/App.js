93e7ecfe491a4f977cf6b10ecd10264a24aa0423
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-09-16 08:11:36 +0530

    Added routing path with react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { Details } from './pages/Details';
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/details">
+            <Details />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
