528afd6026df27945516b6e1f47c6ee65b5bc68b
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-09-17 07:58:50 +0530

    Added desktop style to details page

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -24,70 +24,74 @@ export const CountryDetails = (props) => {
       data.filter((ac) => ac.alpha3Code === bc)
     );
     const markup = borderCountriesList.map((c) => (
-      <button
-        key={c.alpha3Code}
-        className="bg-theme-elements rounded shadow px-4 py-2 m-1"
+      <Link
+        to={`/details/${c.alpha3Code}`}
+        className="block  bg-theme-elements rounded shadow px-4 py-2 m-1"
       >
-        <Link to={`/details/${c.alpha3Code}`}>{c.name}</Link>
-      </button>
+        {c.name}
+      </Link>
     ));
     return <>{markup}</>;
   };
   return (
-    <>
-      <div className="flag">
+    <div className="lg:flex justify-between gap-20 my-10">
+      <div className="lg:w-1/2">
         <img
           src={countryRequested.flag}
           alt={'Flag of ' + countryRequested.name}
         />
       </div>
-      <div className="country-details">
-        <h2 className="my-4 text-xl">{countryRequested.name}</h2>
-        <div className="">
-          <div className="my-2">
-            <p className="my-1">
-              <span className="bold">Native Name: </span>
-              {countryRequested.nativeName}
-            </p>
-            <p className="my-1">
-              <span className="bold">Population: </span>
-              {countryRequested.population}
-            </p>
-            <p className="my-1">
-              <span className="bold">Region: </span>
-              {countryRequested.region}
-            </p>
-            <p className="my-1">
-              <span className="bold">Sub Region: </span>
-              {countryRequested.subregion}
-            </p>
-            <p className="my-1">
-              <span className="bold">Capital: </span>
-              {countryRequested.capital}
-            </p>
-          </div>
-          <div className="my-4">
-            <p className="my-1">
-              <span className="bold">Top Level Domain: </span>
-              {countryRequested.topLevelDomain}
-            </p>
-            <p className="my-1">
-              <span className="bold">Currencies: </span>
-              {countryRequested.currencies[0].code}
-            </p>
-            <p className="my-1">
-              <span className="bold">Languages: </span>
-              {countryRequested.languages
-                .map((language) => language.name)
-                .join(', ')}
-            </p>
+      <div className="lg:w-1/2">
+        <div className="m-2">
+          <h2 className="my-4 text-xl lg:text-4xl">{countryRequested.name}</h2>
+          <div className="lg:flex justify-between">
+            <div className="my-2 lg:m-0">
+              <p className="my-1">
+                <span className="bold">Native Name: </span>
+                {countryRequested.nativeName}
+              </p>
+              <p className="my-1">
+                <span className="bold">Population: </span>
+                {countryRequested.population}
+              </p>
+              <p className="my-1">
+                <span className="bold">Region: </span>
+                {countryRequested.region}
+              </p>
+              <p className="my-1">
+                <span className="bold">Sub Region: </span>
+                {countryRequested.subregion}
+              </p>
+              <p className="my-1">
+                <span className="bold">Capital: </span>
+                {countryRequested.capital}
+              </p>
+            </div>
+            <div className="my-4 lg:m-0">
+              <p className="my-1">
+                <span className="bold">Top Level Domain: </span>
+                {countryRequested.topLevelDomain}
+              </p>
+              <p className="my-1">
+                <span className="bold">Currencies: </span>
+                {countryRequested.currencies[0].code}
+              </p>
+              <p className="my-1">
+                <span className="bold">Languages: </span>
+                {countryRequested.languages
+                  .map((language) => language.name)
+                  .join(', ')}
+              </p>
+            </div>
           </div>
         </div>
+        <section className="lg:flex m-2 lg:my-10">
+          <h4 className="text-lg">Border Countries: </h4>
+          <nav className="lg:flex items-center flex-wrap">
+            {renderBorderCountries()}
+          </nav>
+        </section>
       </div>
-      <div className="ld:flex">
-        <h4 className="text-lg">Border Countries: </h4>
-        <div>{renderBorderCountries()}</div>
-      </div>
-    </>
+    </div>
   );
 };
